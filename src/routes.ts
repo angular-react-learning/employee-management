993b215f105e45ec247731bb5d5cbe04bd8f93ee
Praveen <praveen@example.com>
2020-03-31 21:48:53 +0530

    uncommented form

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from "@angular/router";
 
-// import { EmployeeFormComponent } from "./app/admin/employee-form/employee-form.component";
-// import { EmployeeListComponent } from "./app/users/employee-list/employee-list.component";
-// import { EmployeeDetailComponent } from "./app/employee-detail/employee-detail.component"
+import { EmployeeFormComponent } from "./app/admin/employee-form/employee-form.component";
+import { EmployeeListComponent } from "./app/users/employee-list/employee-list.component";
+import { EmployeeDetailComponent } from "./app/employee-detail/employee-detail.component"
 
-import { InfoComponent } from "./app/info/info.component" 
+import { InfoComponent } from "./app/info/info.component"
 
-export const routes : Routes = [
-    { path : 'home', component : InfoComponent },
-    // { path : "listing", component : EmployeeListComponent },
-    // { path : "employee/:employeename", component : EmployeeDetailComponent },
-    { path : "", redirectTo : '/home', pathMatch : 'full'}
+export const routes: Routes = [
+    { path: 'covid', component: InfoComponent },
+    { path: "listing", component: EmployeeListComponent },
+    { path: "employee/:employeename", component: EmployeeDetailComponent },
+    { path: "home", component: EmployeeFormComponent },
+    { path: "", redirectTo: '/home', pathMatch: 'full' }
 ]
 
- 
